011f2970bab49629ff38e1bc5cc2df9db9d99766
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2024-12-06 11:16:31 +0000

    Added roundCounter and fixed npcChoice selection
    npcChoice selection is now correctly re-assigning between rounds.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 // Our MS1
 // Global Variables list
 
-playerScore = 0;
-npcScore = 0;
-result = null;
+let playerScore = 0;
+let npcScore = 0;
+let result = null;
+let roundCounter = 0;
 
 // --- Welcome to rock paper scissors ---
 // An console/alert message that welcomes the user to the game
@@ -74,16 +75,19 @@ function roundResult () {  // no parameters required now playerChoice and npcCho
 
     if (playerChoice.toLowerCase() == npcChoice) {
         result = "Draw";
+        roundCounter ++;
     }
     else if ((playerChoice.toLowerCase() === "rock" && npcChoice === "paper") || 
             (playerChoice.toLowerCase() === "scissors" && npcChoice === "rock") ||
             (playerChoice.toLowerCase() === "paper" && npcChoice === "scissors")) {
             result = "Lose";
             npcScore ++;
+            roundCounter ++;
     }
     else {
         result = "Win";
         playerScore ++;
+        roundCounter ++;
     }
 }
 
@@ -98,7 +102,7 @@ console.log(`%cYou chose: ${playerChoice} | %cI chose: ${npcChoice} | %cYou ${re
 // Update scores accordingly
 // Taken from the return values of the comparison function show the score of 1 for the winner, and 0 for the loser
 // Message that includes ${playerScore} and ${npcScore}
-console.log(`Round : \n Player Score: ${playerScore} | I Scored: ${npcScore}`);
+console.log(`Round ${roundCounter} : \n Player Score: ${playerScore} | I Scored: ${npcScore}`);
 
 // Play again? - prompt
 // variable anotherGame (storing boolean yes/no)
@@ -110,10 +114,10 @@ function playAgain() {
     anotherGame = prompt("Play again? Y or N"); 
     if(anotherGame.toUpperCase() === "Y") {
         firstPrompt();
-        npcRandom();
+        npcChoice = npcRandom();
         roundResult();
         console.log(`%cYou chose: ${playerChoice} | %cI chose: ${npcChoice} | %cYou ${result}!`, "color:green;", "color:red;","color: yellow;" );
-        console.log(`Round : \n Player Score: ${playerScore} | I Scored: ${npcScore}`);
+        console.log(`Round ${roundCounter} : \n Player Score: ${playerScore} | I Scored: ${npcScore}`);
         playAgain();
         } 
         else {
